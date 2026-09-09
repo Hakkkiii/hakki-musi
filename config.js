a0f72e0a0c59d19117099c5a776d4c40b5141b01
Hakkkiii <162152748+Hakkkiii@users.noreply.github.com>
2026-09-09 23:02:45 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,258 @@
+const HAKKI_CONFIG = {
+    "visuals": {
+        "theme": "#3b3b3b",
+        "bg": "#ffffff",
+        "card": "#696969",
+        "cardOpacity": "7",
+        "text": "#000000",
+        "glow": "96",
+        "layout": "sidebar",
+        "cardShape": "cyber",
+        "mediaLayout": "list",
+        "playerStyle": "cyber",
+        "bgEngine": "3",
+        "density": "200",
+        "palette": [
+            "#000000",
+            "#4d4d4d",
+            "#828282",
+            "#9ca3af"
+        ],
+        "globalBgUrl": "blob:file:///39dd28ff-5004-43fe-a6fc-9048e02a9546",
+        "globalBgType": "image",
+        "bgDbId": "global_bg_1788965998179",
+        "bgOverlayOpacity": "4"
+    },
+    "infoBlocks": [
+        {
+            "id": 1,
+            "label": "Creator",
+            "value": "Hakki",
+            "isTitle": true,
+            "children": [
+                "音乐制作人 /Sound Design"
+            ]
+        },
+        {
+            "id": 3,
+            "label": "Contact",
+            "value": "WeChat: Hakki0816",
+            "isTitle": false,
+            "children": [
+                "Phone:16536660816",
+                "Email:794021576@qq.com"
+            ]
+        }
+    ],
+    "categoryBlocks": [
+        {
+            "id": "c1",
+            "name": "电子音乐"
+        },
+        {
+            "id": "c1788941602734",
+            "name": "管弦乐"
+        }
+    ],
+    "mediaFiles": [
+        {
+            "id": 1788959605816,
+            "type": "audio",
+            "title": "Teto Garage",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\Teto Garage Clip.mp3",
+            "dbId": "media_1788959605811",
+            "category": "电子音乐",
+            "notes": [
+                "Garage"
+            ],
+            "cardColor": "#8b5cf6",
+            "particleEngine": 0,
+            "particleColors": [
+                "#8b5cf6",
+                "#ff6179"
+            ]
+        },
+        {
+            "id": 1788941930030,
+            "type": "audio",
+            "title": "Crystal Spiral",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\Astral Bass & complextro.mp3",
+            "dbId": "media_1788941930022",
+            "category": "电子音乐",
+            "notes": [
+                "融合电子乐"
+            ],
+            "cardColor": "#9162fe",
+            "particleEngine": 13,
+            "particleColors": [
+                "#00abb8",
+                "#828282"
+            ]
+        },
+        {
+            "id": 1788941688821,
+            "type": "audio",
+            "title": "灰烬之声",
+            "author": "Hakki/Ember",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\20260602 95BPM Emin 2.mp3",
+            "dbId": "media_1788941688815",
+            "category": "电子音乐",
+            "notes": [
+                "Drum & bass Jungle"
+            ],
+            "cardColor": "#000000",
+            "particleEngine": 10,
+            "particleColors": [
+                "#212121",
+                "#636363"
+            ]
+        },
+        {
+            "id": 1788942621889,
+            "type": "audio",
+            "title": "Midnight Kissa",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\20260520 变拍子.mp3",
+            "dbId": "media_1788942621883",
+            "category": "电子音乐",
+            "notes": [
+                "Garege"
+            ],
+            "cardColor": "#8b5cf6",
+            "particleEngine": 14,
+            "particleColors": [
+                "#ff7070",
+                "#b8f4ff"
+            ]
+        },
+        {
+            "id": 1788942746142,
+            "type": "audio",
+            "title": "Neon Synapse",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\20260518 jazz fusion.mp3",
+            "dbId": "media_1788942746136",
+            "category": "电子音乐",
+            "notes": [
+                "Garage/Hi-tech"
+            ],
+            "cardColor": "#fe7c7c",
+            "particleEngine": 4,
+            "particleColors": [
+                "#ffbf75",
+                "#ff8a8a"
+            ]
+        },
+        {
+            "id": 1788942911947,
+            "type": "audio",
+            "title": "Complextro Remix",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\Complextro Remix.mp3",
+            "dbId": "media_1788942911325",
+            "category": "电子音乐",
+            "notes": [
+                "Complextro"
+            ],
+            "cardColor": "#8b5cf6",
+            "particleEngine": 8,
+            "particleColors": [
+                "#ad8bfe",
+                "#f9ffa8"
+            ]
+        },
+        {
+            "id": 1788942996610,
+            "type": "audio",
+            "title": "Magical Ribbon, Midnight Orbit",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\kawaii bass.mp3",
+            "dbId": "media_1788942995989",
+            "category": "电子音乐",
+            "notes": [
+                "Kawaii Bass"
+            ],
+            "cardColor": "#f75fce",
+            "particleEngine": 7,
+            "particleColors": [
+                "#bca1fc",
+                "#fc9afe"
+            ]
+        },
+        {
+            "id": 1788943077054,
+            "type": "audio",
+            "title": "鸣潮 - Boss 战 - 燃梦的黑炎-SS激斗等级",
+            "author": "hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\Drums&bass（鸣潮战斗）.wav",
+            "dbId": "media_1788943076412",
+            "category": "电子音乐",
+            "notes": [
+                "Drum&bass"
+            ],
+            "cardColor": "#000000",
+            "particleEngine": 11,
+            "particleColors": [
+                "#000000",
+                "#ff5c5c"
+            ]
+        },
+        {
+            "id": 1788945998476,
+            "type": "audio",
+            "title": "听雪",
+            "author": "Hakki",
+            "url": "C:\\Hakki-extension\\hakki-web\\assets\\china 听雪.mp3",
+            "dbId": "media_1788945998459",
+            "category": "管弦乐",
+            "notes": [
+                "中国风"
+            ],
+            "cardColor": "#fe8686",
+            "particleEngine": 12,
+            "particleColors": [
+                "#6b6b6b",
+                "#4f4f4f"
+            ]
+        }
+    ],
+    "soundEffects": {
+        "click": [
+            {
+                "id": "sfx_click_1788965170937_515",
+                "name": "Hakki Percussion 1.wav"
+            },
+            {
+                "id": "sfx_click_1788965170942_436",
+                "name": "Hakki Percussion 4.wav"
+            }
+        ],
+        "hover": [
+            {
+                "id": "sfx_hover_1788965178717_820",
+                "name": "悬浮 1.wav"
+            },
+            {
+                "id": "sfx_hover_1788965178722_592",
+                "name": "悬浮 2.wav"
+            },
+            {
+                "id": "sfx_hover_1788965178725_837",
+                "name": "悬浮 3.wav"
+            }
+        ],
+        "nav": [
+            {
+                "id": "sfx_nav_1788965185739_830",
+                "name": "Hakki Percussion 8.wav"
+            },
+            {
+                "id": "sfx_nav_1788965185744_129",
+                "name": "Hakki Percussion 9.wav"
+            }
+        ],
+        "volume": "0.25"
+    }
+};
